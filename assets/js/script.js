7216998b54fbbb9f5834c2521b116ba1021c6003
Begüm Ircı <begumirci@Begum-MacBook-Air.local>
2024-04-28 12:26:14 +0300

    bazı şeyler düzeltildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,73 @@
-const dataTable = document.querySelector(".table")
-const kucukler = document.querySelector(".kucukler")
-const inappropriateBtn = document.querySelector(".inappropriateBtn")
-const appropriateBtn = document.querySelector(".appropriate")
+const dataTable = document.querySelector('.table');
+const kucukler = document.querySelector('.kucukler');
+const inappropriateBtn = document.querySelector('.inappropriateBtn');
+const appropriateBtn = document.querySelector('.appropriateBtn');
 
-async function getData(){
-    const response = await fetch("/assets/json/data.json")
-    const data = await response.json();
-    return data
+async function getData() {
+  const response = await fetch('/assets/json/data.json');
+  const data = await response.json();
+  return data;
 }
 
 const date = new Date();
 
-async function listData(){
-    const data = await getData();
-    data.forEach(student => {
+async function listData() {
+  const data = await getData();
+  console.log(data);
+  data.forEach((student) => {
+    student.age = date.getFullYear() - student.dogumTarihi;
+
+    dataTable.innerHTML += `
+        <tr>
+            <td>${student.ad}</td>
+            <td>${student.soyad}</td>
+            <td>${student.age}</td>
+            <td>${student.sehir}</td>
+        </tr>
+
+        `;
+  });
+
+  const age = data.map((student) => date.getFullYear() - student.dogumTarihi);
+  const inappropriates = age.filter((studeAge) => studeAge < 20);
+  const appropriates = age.filter((studeAge) => studeAge > 20);
+
+  appropriateBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    dataTable.innerHTML = '';
+    data.forEach((student) => {
+      student.age = date.getFullYear() - student.dogumTarihi;
+      if (student.age > 20) {
         dataTable.innerHTML += `
         <tr>
             <td>${student.ad}</td>
             <td>${student.soyad}</td>
-            <td>${date.getFullYear() - student.dogumTarihi}</td>
+            <td>${student.age}</td>
             <td>${student.sehir}</td>
         </tr>
 
-        `
-
-        
-        
+        `;
+      }
     });
-    
-    const age = data.map(student => date.getFullYear() - student.dogumTarihi)
-    const inappropriates = age.filter(studeAge => studeAge < 20)
-    const appropriates = age.filter(studeAge => studeAge > 20)
+  });
 
-    inappropriateBtn.addEventListener("click", function(e){
-        e.preventDefault();
-        kucukler.innerHTML = ""
-        inappropriates.forEach(inappropriate => {
-            kucukler.innerHTML += `
-            <h2>${inappropriate}</h2>
-            `
-        })
+  inappropriateBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    dataTable.innerHTML = '';
+    data.forEach((student) => {
+      student.age = date.getFullYear() - student.dogumTarihi;
+      if (student.age < 20) {
+        dataTable.innerHTML += `
+        <tr>
+            <td>${student.ad}</td>
+            <td>${student.soyad}</td>
+            <td>${student.age}</td>
+            <td>${student.sehir}</td>
+        </tr>
 
-    })
-
-
-
+        `;
+      }
+    });
+  });
 }
 listData();
-
